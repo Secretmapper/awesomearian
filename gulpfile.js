5dc9924c620e614286bfb8dc769d950800954bb4
Arian Allenson M. Valdez <Secretmapper16@gmail.com>
2016-01-24 23:26:23 +0800

    Add webpack support

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,5 @@
 var gulp = require('gulp')
+var webpack = require('gulp-webpack');
 var stylus = require('gulp-stylus')
 var sourcemaps = require('gulp-sourcemaps')
 var minify = require('gulp-minify')
@@ -12,7 +13,13 @@ var fn = {
       .pipe(gulp.dest('./static/css/'))
   },
   js () {
-    gulp.src('./app/styles/main.js')
+    gulp.src('./app/js/main.js')
+      .pipe(webpack({
+        watch: true,
+        output: {
+          filename: '[name].js'
+        }
+      }))
       .pipe(minify())
       .pipe(gulp.dest('./static/js/'))
   }
@@ -23,7 +30,7 @@ gulp.task('js', fn.js)
 
 gulp.task('watch', () => {
   gulp.watch('./app/styles/**/*.styl', ['styles'])
-  gulp.watch('./app/styles/**/*.js', ['js'])
+  gulp.watch('./app/js/**/*.js', ['js'])
 })
 
-gulp.task('default', ['styles', 'watch']);
+gulp.task('default', ['styles', 'js', 'watch']);
